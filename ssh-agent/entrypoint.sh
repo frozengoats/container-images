5d378d58a663ddeff7fd6a8afc4f8bba2bf51c80
bluefoundry <bluefoundry@proton.me>
2025-05-17 15:44:56 +0900

    fixed perms again (#9)
    Co-authored-by: bluefoundry <bluefoundry@noreply.com>

--- ssh-agent/entrypoint.sh
+++ ssh-agent/entrypoint.sh
@@ -4,12 +4,10 @@ if [ ! -z $SSH_AUTH_SOCK ]
 then
   echo "setting ssh agent socket"
   ssh-agent -a $SSH_AUTH_SOCK
+  chmod 666 $SSH_AUTH_SOCK
 else
   ssh-agent
 fi
 
-eval $(ssh-agent)
-chmod 666 ${SSH_AUTH_SOCK}
 touch /complete
-
 sleep infinity
