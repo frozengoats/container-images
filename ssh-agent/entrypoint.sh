b5c1139dd06431f836c11f3d4810082aa065275e
bluefoundry <bluefoundry@noreply.com>
2025-05-17 15:43:12 +0900

    updated

--- ssh-agent/entrypoint.sh
+++ ssh-agent/entrypoint.sh
@@ -4,8 +4,10 @@ if [ ! -z $SSH_AUTH_SOCK ]
 then
   echo "setting ssh agent socket"
   ssh-agent -a $SSH_AUTH_SOCK
+  chmod 666 $SSH_AUTH_SOCK
 else
   ssh-agent
 fi
 
+touch /complete
 sleep infinity
